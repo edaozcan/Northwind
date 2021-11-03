c1a312bcd0ed41613130152bbd37298a720629ca
edaozcan <73548267+edaozcan@users.noreply.github.com>
2021-11-03 13:58:29 +0300

    .

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
@@ -1,14 +1,27 @@
 package kodlamaio.northwind.api.controllers;
 
+import java.io.ObjectInputStream.GetField;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.FieldError;
+import org.springframework.http.HttpStatus;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.UserService;
 import kodlamaio.northwind.core.entities.User;
+import kodlamaio.northwind.core.utilities.results.ErrorDataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 
 @RestController
@@ -24,11 +37,18 @@ public class UsersController {
 	}
 	
 	@PostMapping(value="/add")
-	public ResponseEntity<?> add( @RequestBody User user) {
+	public ResponseEntity<?> add( @Valid @RequestBody User user) {
 		
 		return ResponseEntity.ok(this.userService.add(user)) ;
 	}
-	public ErrorDataResult<object> handleValidationException(){
-		
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
+		Map<String,String>validationErrors=new HashMap<String,String>();
+		for(FieldError fieldError:exceptions.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
+		}
+		ErrorDataResult<Object>errors=new ErrorDataResult<Object>(validationErrors,"doğrulama hataları");
+		return errors;
 	}
 }
